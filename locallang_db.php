680cf0def32a2e68ca27d07e9df58b7ded5f68c2
hwt3 <hwt3@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-16 14:26:26 +0000

    correct svn structure
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sk_linklist/tags/0.7.5@51155 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ locallang_db.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Language labels for database tables/fields belonging to extension "sk_linklist"
+ * 
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'tt_content.tx_sklinklist_view.I.0' => 'output only the links',
+		'tt_content.tx_sklinklist_view.I.1' => 'output the complete tree which is expanded',
+		'tt_content.tx_sklinklist_view.I.2' => 'output only the actual layer to view sublayers',
+		'tt_content.tx_sklinklist_view.I.3' => 'output link search field',
+		'tt_content.tx_sklinklist_view.I.4' => 'output insert of new links mask',
+		'tt_content.tx_sklinklist_view.I.5' => 'output edit links mask',
+		'tt_content.tx_sklinklist_view.I.6' => 'output category editor',
+		'tt_content.tx_sklinklist_view' => 'Which view should I use here?',
+		'tx_sklinklist_categories' => 'Link category',
+		'tx_sklinklist_categories.category' => 'Category',
+		'tx_sklinklist_categories.subcategory' => 'Subcategory of',
+		'tx_sklinklist_links' => 'Links',
+		'tx_sklinklist_links.url' => 'URL',
+		'tx_sklinklist_links.description' => 'Description',
+		'tx_sklinklist_links.label' => 'label',
+		'tx_sklinklist_links.category' => 'Category',
+		'tx_sklinklist_links.rating' => 'Rating',
+		'tt_content.list_type' => 'Link list',
+	),
+	'dk' => Array (
+	),
+	'de' => Array (
+		'tt_content.tx_sklinklist_view.I.0' => 'nur Links ausgeben',
+		'tt_content.tx_sklinklist_view.I.1' => 'komplett ausgeklappten Baum ausgeben',
+		'tt_content.tx_sklinklist_view.I.2' => 'nur aktuelle Ebene ausgeben, um Unterkategorien zu sehen',
+		'tt_content.tx_sklinklist_view.I.3' => 'Suchfeld ausgeben',
+		'tt_content.tx_sklinklist_view.I.4' => 'Maske zum Eingeben neuer Links',
+		'tt_content.tx_sklinklist_view.I.5' => 'Eingabefelder zum Editieren von Links ausgeben',
+		'tt_content.tx_sklinklist_view.I.6' => 'Kategoeieeditor ausgeben',
+		'tt_content.tx_sklinklist_view' => 'Welche Ansicht soll dargestellt werden?',
+		'tx_sklinklist_categories' => 'Linkkategorie',
+		'tx_sklinklist_categories.category' => 'Kategorie',
+		'tx_sklinklist_categories.subcategory' => 'Unterkategorie von',
+		'tx_sklinklist_links' => 'Links',
+		'tx_sklinklist_links.url' => 'URL',
+		'tx_sklinklist_links.description' => 'Beschreibung',
+		'tx_sklinklist_links.label' => 'Label',
+		'tx_sklinklist_links.category' => 'Kategorie',
+		'tx_sklinklist_links.rating' => 'Bewertung',
+		'tt_content.list_type' => 'Linkliste',
+	),
+	'no' => Array (
+	),
+	'it' => Array (
+		'tt_content.tx_sklinklist_view.I.0' => 'mostra solo i link',
+		'tt_content.tx_sklinklist_view.I.1' => 'mostra l\'albero completo',
+		'tt_content.tx_sklinklist_view.I.2' => 'mostra solo il livello corrente',
+		'tt_content.tx_sklinklist_view.I.3' => 'mostra il box per la ricerca',
+		'tt_content.tx_sklinklist_view.I.4' => 'mostra la maschera per l\'inserimento di nuovi link',
+		'tt_content.tx_sklinklist_view.I.5' => 'mostra la maschera per la modifica dei link',
+		'tt_content.tx_sklinklist_view.I.6' => 'mostra l\'editor delle categorie',
+		'tt_content.tx_sklinklist_view' => 'Cosa deve essere visualizzato?',
+		'tx_sklinklist_categories' => 'Categoria link',
+		'tx_sklinklist_categories.category' => 'Categoria',
+		'tx_sklinklist_categories.subcategory' => 'Sottocategoria di',
+		'tx_sklinklist_links' => 'Link',
+		'tx_sklinklist_links.url' => 'URL',
+		'tx_sklinklist_links.description' => 'Descrizione',
+		'tx_sklinklist_links.label' => 'etichetta',
+		'tx_sklinklist_links.category' => 'Categoria',
+		'tx_sklinklist_links.rating' => 'Voto',
+		'tt_content.list_type' => 'Elenco link',
+	),
+	'fr' => Array (
+	),
+	'es' => Array (
+	),
+	'nl' => Array (
+	),
+	'cz' => Array (
+		'tt_content.tx_sklinklist_view.I.0' => 'pouze linky',
+		'tt_content.tx_sklinklist_view.I.1' => 'kompletní rozbalený strom',
+		'tt_content.tx_sklinklist_view.I.2' => 'pouze aktuální vrstvu pro náhled podøízených',
+		'tt_content.tx_sklinklist_view.I.3' => 'pole hledání linku',
+		'tt_content.tx_sklinklist_view.I.4' => 'masku vložení nového linku',
+		'tt_content.tx_sklinklist_view.I.5' => 'masku úpravy linku',
+		'tt_content.tx_sklinklist_view.I.6' => 'editor kategorií',
+		'tt_content.tx_sklinklist_view' => 'Jaký pøehled mám zde použít?',
+		'tx_sklinklist_categories' => 'Kategorie linku',
+		'tx_sklinklist_categories.category' => 'Kategorie',
+		'tx_sklinklist_categories.subcategory' => 'Podkategorie',
+		'tx_sklinklist_links' => 'Linky',
+		'tx_sklinklist_links.url' => 'URL',
+		'tx_sklinklist_links.description' => 'Popis',
+		'tx_sklinklist_links.label' => 'jmenovka',
+		'tx_sklinklist_links.category' => 'Kategorie',
+		'tx_sklinklist_links.rating' => 'Hodnocení',
+		'tt_content.list_type' => 'Seznam linkù',
+	),
+	'pl' => Array (
+	),
+	'si' => Array (
+	),
+	'fi' => Array (
+		'tt_content.tx_sklinklist_view.I.0' => 'tilosta ainoastaan linkit',
+		'tt_content.tx_sklinklist_view.I.1' => 'tulosta koko puurakenne laajennettuna',
+		'tt_content.tx_sklinklist_view.I.2' => 'tulosta ainoastaan haluttu taso nähdäksesi alatasot',
+		'tt_content.tx_sklinklist_view.I.3' => 'tulosta linkin hakukenttä',
+		'tt_content.tx_sklinklist_view.I.4' => 'tulosta linkin lisäysmaski',
+		'tt_content.tx_sklinklist_view.I.5' => 'tulosta linkin muokkausmaski',
+		'tt_content.tx_sklinklist_view.I.6' => 'tulosta luokan muokkaus',
+		'tt_content.tx_sklinklist_view' => 'Mitä luokkaa tässä käytetään',
+		'tx_sklinklist_categories' => 'Linkin luokka',
+		'tx_sklinklist_categories.category' => 'Luokka',
+		'tx_sklinklist_categories.subcategory' => 'Alaluokka',
+		'tx_sklinklist_links' => 'Linkit',
+		'tx_sklinklist_links.url' => 'URL',
+		'tx_sklinklist_links.description' => 'Kuvaus',
+		'tx_sklinklist_links.label' => 'tunnus',
+		'tx_sklinklist_links.category' => 'Luokka',
+		'tx_sklinklist_links.rating' => 'Luokittelu',
+		'tt_content.list_type' => 'Linkkilista',
+	),
+	'tr' => Array (
+	),
+	'se' => Array (
+	),
+	'pt' => Array (
+	),
+	'ru' => Array (
+	),
+	'ro' => Array (
+	),
+	'ch' => Array (
+	),
+	'sk' => Array (
+	),
+	'lt' => Array (
+	),
+	'is' => Array (
+	),
+	'hr' => Array (
+	),
+	'hu' => Array (
+	),
+	'gl' => Array (
+	),
+	'th' => Array (
+	),
+	'gr' => Array (
+	),
+	'hk' => Array (
+	),
+	'eu' => Array (
+	),
+	'bg' => Array (
+	),
+	'br' => Array (
+		'tt_content.tx_sklinklist_view.I.0' => 'exibir apenas os links',
+		'tt_content.tx_sklinklist_view.I.1' => 'exibir a árvore completa, expandida',
+		'tt_content.tx_sklinklist_view.I.2' => 'exibir apenas a camada atual para ver subcamadas',
+		'tt_content.tx_sklinklist_view.I.3' => 'exibir o campo de busca de links',
+		'tt_content.tx_sklinklist_view.I.4' => 'exibir a máscara de inserção de novos links',
+		'tt_content.tx_sklinklist_view.I.5' => 'exibir a máscara para edição de links',
+		'tt_content.tx_sklinklist_view.I.6' => 'exibir o editor de categorias',
+		'tt_content.tx_sklinklist_view' => 'Qual modo de exibição devo usar aqui?',
+		'tx_sklinklist_categories' => 'Categoria de links',
+		'tx_sklinklist_categories.category' => 'Categoria',
+		'tx_sklinklist_categories.subcategory' => 'Subcategoria de',
+		'tx_sklinklist_links' => 'Links',
+		'tx_sklinklist_links.url' => 'URL',
+		'tx_sklinklist_links.description' => 'Descrição',
+		'tx_sklinklist_links.label' => 'rótulo',
+		'tx_sklinklist_links.category' => 'Categoria',
+		'tx_sklinklist_links.rating' => 'Classificação',
+		'tt_content.list_type' => 'Lista de links',
+	),
+	'et' => Array (
+	),
+	'ar' => Array (
+	),
+	'he' => Array (
+	),
+	'ua' => Array (
+	),
+	'lv' => Array (
+	),
+	'jp' => Array (
+	),
+	'vn' => Array (
+	),
+);
+?>
